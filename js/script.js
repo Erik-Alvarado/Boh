f7fe488bd670b23a143cc0f44db10461b259c43d
Erik-Alvarado <erik.alvarado93@yahoo.com>
2021-07-26 22:40:44 -0400

    setup carosel arrows, need to fine tune js. small changes to logo size

--- js/script.js
+++ js/script.js
@@ -2,48 +2,44 @@
 let didScroll;
 let lastScrollTop = 0;
 const delta = 5;
-const navbarHeight = $('header').outerHeight();
+const navbarHeight = $("header").outerHeight();
 
 $(window).scroll(function (event) {
-    didScroll = true;
+  didScroll = true;
 });
 
 setInterval(function () {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
 function hasScrolled() {
-    const st = $(this).scrollTop();
+  const st = $(this).scrollTop();
 
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta) return;
 
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if (st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $("header").removeClass("nav-down").addClass("nav-up");
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $("header").removeClass("nav-up").addClass("nav-down");
     }
+  }
 
-    lastScrollTop = st;
+  lastScrollTop = st;
 }
 
 const hamburger = document.querySelector(".ham-menu");
 const navMenu = document.querySelector(".menu-nav");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
- 
-   
-
